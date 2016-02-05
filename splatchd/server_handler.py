efd1939e7df1ff59d97750445226b2d72264c3a7
Jack <jackman.pierre@gmail.com>
2016-02-05 02:05:47 +0100

    authenticate pblic keys

--- splatchd/server_handler.py
+++ splatchd/server_handler.py
@@ -1,8 +1,12 @@
+
+import os
 import logging
 import threading
-
+import base64
 import paramiko
 import database
+from binascii import hexlify
+from paramiko.py3compat import b, u, decodebytes
 
 LOG = logging.getLogger('spatch')
 
@@ -17,31 +21,26 @@ def query_server_selection(granted_servers):
         L.append(server_list_fmt.format(hostname=server[0],
                                         port=server[1], alias=server[2]))
     info = info.format('\n'.join(L))
-    q = paramiko.InteractiveQuery("", info)
+    query = paramiko.InteractiveQuery("", info)
 
-    q.add_prompt("Enter the server's hostname: ", echo=True)
-    return q
+    query.add_prompt("Enter the server's hostname: ", echo=True)
+    return query
 
 
 class ServerHandler(paramiko.ServerInterface):
 
     def __init__(self):
-        self._allowed = set(['keyboard-interactive', 'publickey', 'password'])
+        self._allowed_auths = ['keyboard-interactive']
 
         self._shell_request_event = threading.Event()
         
+    def get_allowed_auths(self, username):
+        return ','.join(self._allowed_auths)
+
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
-        
-    def check_auth_password(self, username, password):
-
-        # db here
-        if (username == 'jack') and (password == 'foo'):
-            return paramiko.AUTH_SUCCESSFUL
-            # return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_FAILED
 
     def check_auth_interactive(self, username, submethods):
         self._granted = database.granted_servers(username)
@@ -52,19 +51,27 @@ class ServerHandler(paramiko.ServerInterface):
     def check_auth_interactive_response(self, responses):
         for host, _, alias in self._granted:
             if host == responses[0]:
-                self._allowed.pop()
+                self._allowed_auths = ['publickey']
                 return paramiko.AUTH_PARTIALLY_SUCCESSFUL
+        # self._allowed_auths = []
         return paramiko.AUTH_FAILED
     
     def check_auth_publickey(self, username, key):
-        # print('Auth attempt with key: ' + u(hexlify(key.get_fingerprint())))
-        # load db
-        if (username == 'robey') and (key == self.good_pub_key):
-            return paramiko.AUTH_SUCCESSFUL
+        pub_key_filename = database.user(username).public_key_filename
+        try:
+            LOG.info("checking %s" % pub_key_filename)
+            assert os.path.exists(pub_key_filename)
+            with open(pub_key_filename, 'rb') as pubkey:
+                pubkey_data = pubkey.read().split(' ')[1]
+
+        except paramiko.SSHException as e:
+            LOG.error(e)
+            return paramiko.AUTH_FAILED
+        else:
+            if key.get_base64() == pubkey_data:
+                LOG.info("user successfully authed publickey")
+                return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
-        
-    def get_allowed_auths(self, username):
-        return ','.join(self._allowed)
 
     def check_channel_shell_request(self, channel):
         self._shell_request_event.set()
